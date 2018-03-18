10dbb8da4fc786aafdd7ac05f1fc6fae91402639
Diego Sousa <diegors90@gmail.com>
2018-03-18 16:57:52 -0300

    adicionado testes gerados pela ferramenta evosuite

--- SistemaParaMonitoramentoDaPressaoDePneus/src/com/uepb/tcc/racingcar/Alarme.java
+++ SistemaParaMonitoramentoDaPressaoDePneus/src/com/uepb/tcc/racingcar/Alarme.java
@@ -5,7 +5,7 @@ public class Alarme {
 	private final double limiteBaixaPressao = 17;
 	private final double limiteAltaPressao = 21;
 
-	Sensor sensor = new Sensor();
+	public Sensor sensor = new Sensor();
 
 	boolean alarmeLigado = false;
 
